555a755668ad211804388ce961ef9fd8ace38417
s4664845 <100103717+s4664845@users.noreply.github.com>
2022-08-14 22:32:27 +1000

    Add files via upload

--- Assessment1 part1.R
+++ Assessment1 part1.R
@@ -3,25 +3,26 @@ library(rtweet)
 
 auth_setup_default()
 
-football_tweets<-search_tweets(
+football_tweets1<-search_tweets(
   q="football",
   n=15000,
   include_rts = FALSE,
   lang="en",
   retryonratelimit = TRUE)
 
-football_tweets<-football_tweets%>%flatten()
+football_tweets1<-football_tweets%>%flatten()
 
-football_tweets%>%write_csv("12_08_2022.csv")
+football_tweets1%>%write_csv("12_08_2022.csv")
 
-football_tweets%>%write_csv("13_08_2022.csv")
+football_tweets2%>%write_csv("13_08_2022.csv")
 
-football_tweets%>%write_csv("14_08_2022.csv")
+football_tweets3%>%write_csv("14_08_2022.csv")
 
 #merging and opening all csv files
 
 files<-list.files(pattern="\\.csv$",full.names = TRUE) 
-all_data<-map_df(files, ~read_csv(.x))
+
+all_data<-map_df(files,  ~read_csv(.x))
 
 #remove duplicate entries
 
